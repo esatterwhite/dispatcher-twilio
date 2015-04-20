e138b8ff492bf67f54370ed71ab44c1f3a54470d
Eric Satterwhtie <esatterwhite@wi.rr.com>
2015-04-20 13:49:34 -0500

    remove account creds from conf

--- conf/defaults.js
+++ conf/defaults.js
@@ -9,10 +9,7 @@
  */
 module.exports = {
 	twilio:{
-		user: 'development@dispatchertrucking.com'
-	  , password: 'D1spatcher'
-	  , sid: 'ACfb37bf4634ab94764c5aac4293f31ce3'
+		sid: 'ACfb37bf4634ab94764c5aac4293f31ce3'
 	  , token: '4e937b83d5c8893829ca4538ff77d21b'
-
 	}
 }
