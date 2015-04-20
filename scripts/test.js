06cf4d44de9675db990437a99af6e0297bd45b94
Eric Satterwhtie <esatterwhite@wi.rr.com>
2015-04-20 13:51:16 -0500

    Region tests
    Mocha specs for Region class

---
+++ scripts/test.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+/*jshint laxcomma: true, smarttabs: true*/
+/*globals module,process,require */
+'use strict';
+/**
+ * simple test runner harness
+ * @module scripts/test
+ * @author Eric Satterwhite
+ * @since 0.0.1
+ * @requires child_process
+ * @requires module:mout/lang/clone
+ * @requires fs
+ * @requires path
+ * @requires os
+ * @requires util
+ */
+
+var child_process = require('child_process')               // child proces for spawning mocha
+  , fs            = require('fs')                          // fs module
+  , path          = require('path')                        // path module
+  , os            = require('os')                          // os module
+  , util          = require("util")                        // util module
+  , clone         = require('mout/lang/clone')             // object clone module
+  , debug         = require('debug')( 'scripts:runner')
+  , env           = clone( process.env )                   // clone of current process env
+  , npath         = ( env.NODE_PATH || "" ).split( path.delimiter )     // cache of node path split into an array
+  , html                                                   // html stream
+  , coverage                                               // mocha code coverage process
+  , mocha                                                  // moacha child process
+  , reporter
+  ;
+
+// add our modules director to node require path
+// so we don't have to require( ../../../../../ )
+npath.push(path.resolve(__dirname,'..','packages') )
+npath = npath.join( path.delimiter )
+env.NODE_PATH = npath  
+
+reporter = process.stdout
+process.stdout.write(os.EOL);
+
+// spinner.start();
+// spinner.stop();
+mocha = child_process.spawn("mocha", [
+  "--harmony"
+  , "--growl"
+  , "--recursive"
+  // ,"--debug-brk"
+  ,"--timeout=1200000"
+  , '--reporter=spec'
+  , "test"
+], { env:env });
+
+mocha.on('exit', function( code, sig ){
+  process.exit( code );
+});
+
+mocha.stdout.pipe( reporter )
+mocha.stderr.pipe( reporter )
---
+++ test/region.spec.js
@@ -0,0 +1,109 @@
+var assert = require('assert')
+  , should = require('should')
+  , Region = require("../lib/region")
+  , Promise = require('bluebird')
+  , remove = require('mout/array/remove')
+
+describe('region',function(){
+	describe('#state', function(){
+		
+		it('should resolve the resident state', function(){
+			var region = new Region( 414 );
+
+			region
+				.state()
+				.then(function( state ){
+					state.should.be.String;
+					state.should.equal('wisconsin')
+					region.code = 212
+					return region.state()
+				})
+				.then(function( state ){
+					state.should.equal('new york');
+					region.code = 606
+					return region.state()
+				})
+				.then(function( state ){
+					state.should.equal('kentucky')
+				})
+		});
+	});
+
+	describe('#number',function(){
+		it('should locate a number in the same area code', function( done ){
+			var region = new Region( 414 )
+			region
+				.number()
+				.then( function( data ){
+					var bits = /(\d{3})/.exec( data.friendly_name )
+					bits[1].should.equal('414')
+					done();
+				})
+		});
+
+		it('should exhaust all area codes', function( done ){
+			var codes, region;
+
+			region = new Region( 212 )
+
+			region.on( 'search', function( code ){
+				remove( codes, "" + code )
+			})
+			region
+				.codes( true )
+				.then(function( areacodes ){
+					codes = areacodes;
+					codes.length.should.not.equal( 0 )
+					return region.number();
+				})
+				.catch( function( e ){
+					e.code.should.equal(101)
+					codes.length.should.equal( 0 )
+				})
+		})
+
+	});
+	describe('#codes', function(){
+		var region;
+
+		before(function( done ){
+			region = new Region( 414 );
+			done()
+		});
+
+		it('should return a promise', function( done ){
+			var p = region.codes();
+			assert.ok( p instanceof Promise );
+
+			p.then( function(){
+				done()
+			})	
+		});
+		
+		it('should resolve to an array',function( done ) {
+			region
+				.codes()
+				.then( function( codes ){
+					assert( Array.isArray( codes ))
+					done();
+				});
+		});
+
+		it('should resolve all area codes in the same state',function( done ) {
+			region
+				.codes()
+				.map(function(code){
+					return parseInt( code )
+				})
+				.then(function( codes ){
+					assert(codes.indexOf( 262 ) >= 0 )
+					assert(codes.indexOf( 274 ) >= 0 )
+					assert(codes.indexOf( 534 ) >= 0 )
+					assert(codes.indexOf( 608 ) >= 0 )
+					assert(codes.indexOf( 715 ) >= 0 )
+					assert(codes.indexOf( 920 ) >= 0 )
+					done()
+				})
+		});
+	})
+});
